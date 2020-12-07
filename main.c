197da055b1d10f08e7ff0560a811aab30de09086
Mohammed Esafi <mesafi@student.1337.ma>
2020-12-07 07:50:05 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <GL/glut.h>
+#include <math.h>
+#define pi 3.142857
+
+// function to initialize
+void Init(void)
+{
+	// making background color black as first
+	// 3 arguments all are 0.0
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+
+	// making picture color green (in RGB mode), as middle argument is 1.0
+	glColor3f(0.0, 1.0, 0.0);
+
+	// breadth of picture boundary is 1 pixel
+	glPointSize(1.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	// setting window dimension in X- and Y- direction
+	gluOrtho2D(-780, 780, -420, 420);
+}
+
+void drawing(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POINTS);
+	float x, y, i;
+
+	// iterate y up to 2*pi, i.e., 360 degree
+	// with small increment in angle as
+	// glVertex2i just draws a point on specified co-ordinate
+	for (i = 0; i < (2 * pi); i += 0.001)
+	{
+		// let 200 is radius of circle and as,
+		// circle is defined as x=r*cos(i) and y=r*sin(i)
+		x = 200 * cos(i);
+		y = 200 * sin(i);
+
+		glVertex2i(x, y);
+	}
+	glEnd();
+	glFlush();
+}
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(1366, 768);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("CoreWar");
+	Init();
+
+	glutDisplayFunc(drawing);
+	glutMainLoop();
+}
